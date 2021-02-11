3a68ba61dc14f16898236b24e0238c8f2cc6b38a
dylan-anz <dylan.butler@anz.com>
2021-02-11 15:20:57 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Alpaca Desktop
+
+[![Latest Tag](https://img.shields.io/github/v/tag/jamesmoriarty/alpaca-desktop.svg?logo=github&label=latest)](https://github.com/jamesmoriarty/alpaca-desktop/releases) [![Build Status](https://travis-ci.org/jamesmoriarty/alpaca-desktop.svg?branch=master)](https://travis-ci.org/jamesmoriarty/alpaca-desktop) ![GitHub Releases](https://img.shields.io/github/downloads/jamesmoriarty/alpaca-desktop/total)
+
+Experimental OSX menu bar widget for [Alpaca][2]. Built with [BitBar][1] distribution and includes [Alpaca][2] binary and [Launchd][3] daemon.
+
+![Screenshot](docs/screenshot.jpg)
+
+## Install
+
+```
+Alpaca.app
+```
+Releases can be downloaded from [here][4]. \[1] \[2]
+
+<hr />
+
+\[1] macOS Mojave and below - can be run as untrusted.
+
+\[2] macOS Catalina - will need to be built locally.
+
+## Build
+
+```
+bin/build
+```
+Build artifact can be found at `dist/Alpaca.app`.
+
+## Icon
+
+```
+cat src/icons/icon.png | base64
+```
+
+The image data must be passed as base64.
+
+Application `icns` generated with [IconGenerator][5].
+
+[1]: https://github.com/matryer/bitbar
+[2]: https://github.com/samuong/alpaca
+[3]: https://developer.apple.com/library/archive/documentation/MacOSX/Conceptual/BPSystemStartup/Chapters/CreatingLaunchdJobs.html
+[4]: https://github.com/jamesmoriarty/alpaca-desktop/releases
+[5]: https://github.com/onmyway133/IconGenerator
---
+++ src/alpaca.3s.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require 'base64'
+
+# Configuration
+
+PLIST_NAME     = "com.github.alpaca"
+LOG_PATH       = File.join(Dir.home, "/Library/Logs/#{PLIST_NAME}.log")
+PLIST_PATH     = File.expand_path(File.join(Dir.home, "/Library/LaunchAgents/#{PLIST_NAME}.plist"))
+APP_PATH       = File.join(File.expand_path(File.dirname(File.dirname(__FILE__))), "MacOS")
+ALPACA_PATH    = File.join(APP_PATH, "/bin/alpaca")
+INSTALL_PATH   = File.join(APP_PATH, "/bin/install")
+UNINSTALL_PATH = File.join(APP_PATH, "/bin/uninstall")
+ICON_BASE64    = Base64.strict_encode64(IO.read(File.join(APP_PATH, "/img/icon.png"))).chomp
+
+def running?(plist_name)
+  return `launchctl list #{plist_name}`[/"PID" = (\d+)/, 1]
+end
+
+# Daemon controls
+
+if running? PLIST_NAME
+  puts "| templateImage=#{ICON_BASE64}"
+  puts "---"
+  puts "Daemon is running"
+  puts "Stop | terminal=false bash=/bin/launchctl args=stop__#{PLIST_NAME}"
+elsif File.exists? PLIST_PATH
+  puts "⚠️ | templateImage=#{ICON_BASE64}"
+  puts "---"
+  puts "Daemon is stopped"
+  puts "Start | terminal=false bash=/bin/launchctl args=start__#{PLIST_NAME}"
+else
+  puts "⚠️ | templateImage=#{ICON_BASE64}"
+  puts "---"
+  puts "Install | terminal=false bash=#{INSTALL_PATH} args=#{PLIST_NAME}__#{PLIST_PATH}__#{ALPACA_PATH}__#{LOG_PATH}"
+end
+
+# Log tail
+
+if File.exists? LOG_PATH
+  puts "Logs | terminal=false bash=/usr/bin/open args=#{LOG_PATH}"
+  puts "---"
+  puts `tail -n 3 #{LOG_PATH}`
+end
+
+puts "---"
+puts "Uninstall | terminal=false bash=#{UNINSTALL_PATH} args=#{PLIST_NAME}__#{PLIST_PATH}__#{ALPACA_PATH}__#{LOG_PATH}"
